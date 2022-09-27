fc6f2d3cc528b3661c54a0f61b40604936d8e109
MatheusMed <dutra.md@hotmail.com>
2022-09-27 15:23:23 -0300

    estilizando com o nextJS

--- pages/sobre/index.tsx
+++ pages/sobre/index.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { ButtonComponent } from "../../components/ButtonComponent";
 
+import styles from '../../styles/Sobre.module.css';
+
 function Sobre() {
 
   const [numero, setNumero] = useState(0);
@@ -9,11 +11,15 @@ function Sobre() {
     setNumero(numero + 1);
   }
 
-  return <div>
-    <h1>Pagina Sobre numero: ({numero})</h1>
+  return (
+    <div>
+      <h1 className={styles.sobreTitle} >Pagina Sobre numero: ({numero})</h1>
 
-    <ButtonComponent label="Aumentar" onTap={handleAumentar} />
-  </div>
+      <ButtonComponent label="Aumentar" onTap={handleAumentar} />
+
+
+    </div>
+  );
 }
 
 export default Sobre;
